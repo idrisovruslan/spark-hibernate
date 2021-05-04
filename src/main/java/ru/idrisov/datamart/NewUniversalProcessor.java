fc587078c46371db9cf062fceb9f9b8a74d7ad95
Идрисов Руслан Радикович <RuRaIdrisov@omega.sbrf.ru>
2021-05-04 19:03:22 +0300

    remove useless imports

--- src/main/java/ru/idrisov/datamart/NewUniversalProcessor.java
+++ src/main/java/ru/idrisov/datamart/NewUniversalProcessor.java
@@ -3,24 +3,15 @@ package ru.idrisov.datamart;
 import lombok.RequiredArgsConstructor;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
-import ru.idrisov.domain.annotations.Join;
-import ru.idrisov.domain.annotations.Joins;
-import ru.idrisov.domain.annotations.SourceTableField;
-import ru.idrisov.domain.entitys.TableSpark;
 import ru.idrisov.domain.entitys.TargetTable;
 
-import java.util.*;
-
-import static ru.idrisov.utils.TableUtils.*;
+import static ru.idrisov.utils.TableUtils.saveAsTable;
 
 @Service
 @RequiredArgsConstructor
 public class NewUniversalProcessor {
 
-    final ColumnCreator columnCreator;
     final DataFrameBuilder dataFrameBuilder;
 
     public void fillTable(TargetTable targetTable) {

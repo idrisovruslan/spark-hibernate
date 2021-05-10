ff21131e82ec726025ebfa278d6d66ce18f1aa28
Идрисов Руслан Радикович <RuRaIdrisov@omega.sbrf.ru>
2021-05-10 11:39:37 +0300

    fix annotations RetentionPolicy and Target

--- src/main/java/ru/idrisov/domain/annotations/Aggregate.java
+++ src/main/java/ru/idrisov/domain/annotations/Aggregate.java
@@ -2,6 +2,13 @@ package ru.idrisov.domain.annotations;
 
 import ru.idrisov.domain.enums.AggregateFunctions;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Aggregate {
     AggregateFunctions function();
 }

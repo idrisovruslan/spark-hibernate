af1217efc6fe50ef9fa466c016bd9cc188d93627
Идрисов Руслан Радикович <RuRaIdrisov@omega.sbrf.ru>
2021-05-20 13:25:09 +0300

    added @Deprecated for WherePlace.AFTER_AGGREGATE

--- src/main/java/ru/idrisov/universal_loader/enums/WherePlace.java
+++ src/main/java/ru/idrisov/universal_loader/enums/WherePlace.java
@@ -3,5 +3,8 @@ package ru.idrisov.universal_loader.enums;
 public enum WherePlace {
     BEFORE_JOIN,
     AFTER_JOIN,
+
+    //TODO проверить работоспособность(на 95 процентов она не работает)
+    @Deprecated
     AFTER_AGGREGATE
 }

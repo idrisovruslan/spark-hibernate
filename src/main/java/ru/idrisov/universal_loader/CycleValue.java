ccf34e95011d893835db645211364d145b700593
Идрисов Руслан Радикович <RuRaIdrisov@omega.sbrf.ru>
2021-05-31 15:06:31 +0300

    started CycleValuesHolder

--- src/main/java/ru/idrisov/universal_loader/CycleValue.java
+++ src/main/java/ru/idrisov/universal_loader/CycleValue.java
@@ -5,16 +5,55 @@ import lombok.Setter;
 
 import java.util.List;
 
-@Getter
+
 public class CycleValue {
-    final String mainCycleName;
-    final String mainCycleValue;
+    @Getter
+    private final String mainCycleName;
+
+    @Getter
+    private final String mainCycleValue;
+
     @Setter
-    List<CycleValue> nestedCycleValue;
+    private List<CycleValue> nestedCycleValue;
+
+    @Getter
+    private Boolean processed = false;
 
     public CycleValue(String mainCycleName, String mainCycleValue) {
         this.mainCycleName = mainCycleName;
         this.mainCycleValue = mainCycleValue;
         nestedCycleValue = null;
+    }
+
+    public CycleValue getFirstNestedCycleValueAndRemoveIfLast() {
+        removeNestedCycleValueIfProcessed();
+        setProcessedFlag();
+
+        if (processed) {
+            return null;
+        }
+
+        if (nestedCycleValue.get(0).nestedCycleIsPresent()) {
+            return nestedCycleValue.get(0);
+        }
+        CycleValue result = nestedCycleValue.remove(0);
+        setProcessedFlag();
+        return result;
+    }
+
+    public Boolean nestedCycleIsPresent() {
+        return nestedCycleValue != null;// && nestedCycleValue.size() != 0;
+    }
+
+    public void setProcessedFlag() {
+        if (nestedCycleValue.size() == 0) {
+            processed = true;
+        }
+    }
+
+    public void removeNestedCycleValueIfProcessed() {
+        if (nestedCycleValue.get(0).processed) {
+            nestedCycleValue.remove(0);
+        }
     }
 }
--- src/main/java/ru/idrisov/universal_loader/CycleValuesHolder.java
+++ src/main/java/ru/idrisov/universal_loader/CycleValuesHolder.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.idrisov.universal_loader.entitys.TableSpark;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -17,9 +19,25 @@ public class CycleValuesHolder {
     CycleValue currentCycleValue;
 
 
-    void init(Class<? extends TableSpark> tableInfo) {
+    public void init(Class<? extends TableSpark> tableInfo) {
         cycleValues = cycleValuesCreator.getCycleValuesList(tableInfo);
     }
 
+    private Map<String, String> getNextValues(CycleValue cycleValue) {
+        Map<String, String> result = new HashMap<>();
+        result.put(cycleValue.getMainCycleName(), cycleValue.getMainCycleValue());
+
+        while (cycleValue.nestedCycleIsPresent()) {
+            CycleValue tempCycleValue = cycleValue.getFirstNestedCycleValueAndRemoveIfLast();
+            if (tempCycleValue == null) {
+                continue;
+            }
+            cycleValue = tempCycleValue;
+
+            result.put(cycleValue.getMainCycleName(), cycleValue.getMainCycleValue());
+        }
+
+        return result;
+    }
 
 }
